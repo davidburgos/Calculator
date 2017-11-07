70d57493f16f749f0d9cd4c56ee0d6eeac1caf52
david.burgos <david.burgos@globant.com>
2017-11-07 17:01:13 -0500

    Initial commit

---
+++ app/src/main/java/com/davidburgos/calculator/MainActivity.java
@@ -0,0 +1,15 @@
+package com.davidburgos.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.davidburgos.calculator.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
